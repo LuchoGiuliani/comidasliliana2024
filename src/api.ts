f1b1798b6286696b08e5e64e10e9d2f7d59f48ad
unknown <luciano.giuliani92@gmail.com>
2024-01-29 21:51:23 -0300

    Cambios para manejar el cache

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,31 @@
 import { Menu } from "./types";
-const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTr3_AXLB-lk9-acoDieqmcqIjMeVMzYOS1wmlz7XZuIqnz_X3qgwfRzO9XUzfkKfOeXT2du0UOGGP/pub?output=tsv"
-const timestamp = new Date().getTime();
-const urlWithTimestamp = `${url}?timestamp=${timestamp}`;
+const url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQTr3_AXLB-lk9-acoDieqmcqIjMeVMzYOS1wmlz7XZuIqnz_X3qgwfRzO9XUzfkKfOeXT2du0UOGGP/pub?output=tsv";
+
 const api = {
   menu: {
     list: async (): Promise<Menu[]> => {
-            return fetch(urlWithTimestamp, { next: { tags: ["matches"], }, })
-                .then((res) => res.text())
-      .then((text) => { 
-        return text.split("\n").slice(1).map((row) => {
-          const [id, name, price, isActive] = row.split("\t")
-          return {
-            id: parseInt(price),
-            name,
-            price: parseInt(price),
-            isActive
-          };
+      return fetch(url, {
+        headers: {
+          "Cache-Control": "no-cache",
+        },
+        next: { tags: ["matches"] },
+      })
+        .then((res) => res.text())
+        .then((text) => {
+          return text
+            .split("\n")
+            .slice(1)
+            .map((row) => {
+              const [id, name, price, isActive] = row.split("\t");
+              return {
+                id: parseInt(price),
+                name,
+                price: parseInt(price),
+                isActive,
+              };
+            });
         });
-      });
     },
   },
 };
