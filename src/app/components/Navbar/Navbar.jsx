a8a52f6f8bb6cced7df73c9573c8be098d9d2784
unknown <luciano.giuliani92@gmail.com>
2024-02-06 18:08:37 -0300

    Cambios en el navbar

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
 function Navbar() {
   return (
     <section className="fixed bottom-0 mx-[30%] rounded-t-md bg-[#2E2D2B] p-2">
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center gap-2 ">
         <a href="https://www.instagram.com/luchogiuliani/">
-          <img alt="Instagram" className="mr-4 h-4 w-4" src="/ig.png" />
+          <img alt="Instagram" className=" " src="/ig.png" />
         </a>
         <a href="/">
-          <img alt="WhatsApp" className="mr-4 h-4 w-4" src="/wsp.png" />
+          <img alt="WhatsApp" className=" " src="/wsp.png" />
         </a>
         <a href="/">
-          <img alt="Home" className="mr-4 h-4 w-4" src="/home.png" />
+          <img alt="Home" className=" " src="/home.png" />
         </a>
         <a href="/">
-          <img alt="Next" className="mr-4 h-4 w-4" src="/next.png" />
+          <img alt="Next" className="" src="/next.png" />
         </a>
         <a href="/">
-          <img alt="GitHub" className="h-4 w-4" src="/github.png" />
+          <img alt="GitHub" className="" src="/github.png" />
         </a>
       </div>
     </section>
